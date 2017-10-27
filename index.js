4e60d80ecfa278f361233c109e45e637f61f83d3
Xavier Hocquet <xhocquet@gmail.com>
2017-10-26 21:41:27 -0400

    Persist state so don't have to login

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import * as storage from 'redux-storage'
+import createEngine from 'redux-storage-engine-reactnativeasyncstorage';
 import React, { Component } from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -9,17 +11,26 @@ import thunk from 'redux-thunk'
 import { GroceryGuruNavigator } from './App';
 import AppReducer from './src/reducers/App'
 
+// Storage engine (react-native async)
+const engine = createEngine('GroceryGuruApp');
+
+const reducer = storage.reducer(AppReducer);
+
 const middleware = [ thunk ];
 if (process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger());
+  middleware.push(storage.createMiddleware(engine));
 }
 
-class GroceryGuruApp extends React.Component {
-  store = createStore(AppReducer, applyMiddleware(...middleware));
+const store = createStore(reducer, applyMiddleware(...middleware));
 
+const load = storage.createLoader(engine);
+load(store);
+
+class GroceryGuruApp extends React.Component {
   render() {
     return (
-      <Provider store={this.store}>
+      <Provider store={store}>
         <GroceryGuruNavigator />
       </Provider>
     );
