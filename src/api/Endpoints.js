d4cbce10aa099be1804d8f7420fd921752bf603a
Xavier Hocquet <xhocquet@gmail.com>
2017-10-29 20:17:05 -0400

    Clean up host order

--- src/api/Endpoints.js
+++ src/api/Endpoints.js
@@ -1,6 +1,6 @@
-const host = "http://192.168.1.141:3000/";
+const host = "http://192.168.1.141:3000";
 
-export const receiptList = `${host}receipts.json`;
-export const statsIndex = `${host}stats.json`;
-export const userRegistration = `${host}api/registration`;
-export const userSession = `${host}api/session`;
+export const receiptList = `${host}/api/receipts`;
+export const statsIndex = `${host}/stats.json`;
+export const userRegistration = `${host}/api/registration`;
+export const userSession = `${host}/api/session`;
